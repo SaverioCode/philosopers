06ce6853da9fe8c5eb6ab42b447ff6cd4f9bb775
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-04-12 19:46:32 +0200

    update

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 18:38:34 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/04/12 19:45:44 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/04/12 19:46:26 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ int	main(int ac, char **av)
 	ft_handle_input(ac, av, &philo);
 	ft_create_thread();
 	free(philo.philos);
+	free(philo.forks);
 	return (0);
 }
