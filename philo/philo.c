fdc38a8540271f30355338f16f0a366b17076c64
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-04-12 19:45:56 +0200

    update

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 18:38:34 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/04/12 19:37:07 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/04/12 19:45:44 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	main(int ac, char **av)
 
 	if (ac < 4 || ac > 5)
 		ft_error("Error: input parameters\n");
-	ft_handle_input(ac, av, philo);
-	pthread_create();
+	ft_handle_input(ac, av, &philo);
+	ft_create_thread();
 	free(philo.philos);
 	return (0);
 }
