666e760fc5ad253886cde6aed68bb5748c499760
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-04-17 15:42:50 +0200

    update

--- philo/ft_routine.c
+++ philo/ft_routine.c
@@ -6,11 +6,22 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 16:26:26 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/04/17 14:45:59 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/04/17 15:42:40 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static int	ft_get_id(t_philo *philo)
+{
+	int	id;
+
+	pthread_mutex_lock(philo->data.philo_cnt_mutex);
+	id = philo->data.philo_counter;
+	philo->data.philo_counter++;
+	pthread_mutex_unlock(philo->data.philo_cnt_mutex);
+	return (id);
+}
 
 static int	ft_check_death(t_philo *philo)
 {
@@ -20,6 +31,20 @@ static int	ft_check_death(t_philo *philo)
 	death = philo->data.death;
 	pthread_mutex_unlock(philo->data.death_mutex);
 	return (death);
+}
+
+static int	ft_check_time(t_philo *philo, int time, int id)
+{
+	int	interval;
+
+	gettimeofday(&interval, NULL);
+	if (interval - time >= philo->die_time)
+	{
+		philo->data.death = 0;
+		ft_action(0, philo, id, "died");
+		return (0);
+	}
+	return (1);
 }
 
 static void	ft_action(int time_action, t_philo *philo, int id, char *str)
@@ -36,7 +61,7 @@ static void	ft_action(int time_action, t_philo *philo, int id, char *str)
 		pthread_mutex_lock(&philo->forks[id]);
 	}
 	gettimeofday(time, NULL);
-	printf("%d %d %s\n", philo->time[id] - time, id + 1, str);
+	printf("%d %d %s\n", time - philo->time[id], id + 1, str);
 	usleep(time_action);
 	if (str[3] == 101)
 	{
@@ -47,26 +72,26 @@ static void	ft_action(int time_action, t_philo *philo, int id, char *str)
 
 void	ft_routine(t_philo *philo)
 {
-	int	philo_id;
+	int	id;
 	int	count;
+	int	time;
+	int	time_val;
 
-	pthread_mutex_lock(philo->data.philo_cnt_mutex);
-	philo_id = philo->data.philo_counter;
-	philo->data.philo_counter++;
-	pthread_mutex_unlock(philo->data.philo_cnt_mutex);
+	id = ft_get_id(philo);
 	while (ft_check_death(philo))
 	{
-		if (count == philo->data.max_eat)
+		if (count == 0)
+			gettimeofday(&philo->time[id], NULL);
+		if (count == philo->data.max_eat || !ft_chek_time(philo, time, id))
 			break ;
-		gettimeofday(philo->time[philo_id], NULL);
-		ft_action(philo->eat_time, philo, philo_id, "is eating");
+		ft_action(philo->eat_time, philo, id, "is eating");
+		gettimeofday(&time, NULL);
 		if (!ft_check_death(philo))
 			break ;
-		ft_action(philo->sleep_time, philo, philo_id, "is sleeping");
-		if (!ft_check_death(philo))
+		ft_action(philo->sleep_time, philo, id, "is sleeping");
+		if (!ft_check_death(philo) || !ft_check_time(philo, time, id))
 			break ;
-		ft_action(0, philo, philo_id, "is thinking");
+		ft_action(0, philo, id, "is thinking");
 		count++;
 	}
-	ft_action(0, philo, philo_id, "died");
 }
