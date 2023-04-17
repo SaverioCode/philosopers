8836fef309703d022160401b064c6789c809a5f1
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-04-17 14:36:53 +0200

    udpate

--- philo/ft_create_threads.c
+++ philo/ft_create_threads.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 19:49:04 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/04/13 23:44:43 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/04/17 14:36:37 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,18 @@ void	ft_create_threads(t_philo *philo)
 
 	if (!philo)
 		ft_error("Error: create threads failes\n");
-	i = 0;
 	pthread_mutex_init(philo->data.philo_cnt_mutex, NULL);
 	pthread_mutex_init(philo->data.death_mutex, NULL);
+	i = 0;
+	while (i < philo->data.philo_num)
+	{
+		pthread_mutex_init(&philo->forks[i], NULL);
+		i++;
+	}
+	i = 0;
 	while (i < philo->data.philo_num)
 	{
 		pthread_create(&philo->philos[i], NULL, (void *)ft_routine, philo);
-		pthread_mutex_init(&philo->forks[0], NULL);
 		i++;
 	}
 }
